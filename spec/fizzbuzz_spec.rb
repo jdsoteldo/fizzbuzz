c342861808108ec80a2c82d344d27d56862a5612
JD Soteldo <jdsoteldo00@gmail.com>
2025-08-28 12:48:29 -0400

    init

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../fizzbuzz.rb' 
+
+RSpec.describe "FizzBuzz" do
+  # This is a simple blank test to confirm that RSpec is working.
+
+  it "runs a basic test" do
+    expect(true).to be true
+  end
+end
